9cd27e2c5668d908977754ba9f07338db2a9cad2
timkrans33 <93606550+timkrans33@users.noreply.github.com>
2024-04-07 21:18:43 -0700

    Update NursePortalController.java
    made some fixes with button handling

--- src/Controllers/NursePortalController.java
+++ src/Controllers/NursePortalController.java
@@ -1,8 +1,11 @@
 package Controllers;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import application.ViewFactory;
+import application.PatientFile;
+import application.PatientVisitFile;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -71,9 +74,12 @@ public class NursePortalController {
 
     @FXML
     private TextField weightTF;
+    
+    @FXML
+    private Label errorLabel;
 
     public NursePortalController(){
-    	System.out.print("control");
+    	//.out.print("control");
     }
     
     public void initialize() {
@@ -94,5 +100,78 @@ public class NursePortalController {
 			//System.out.print("clicked");
 			ViewFactory.getViewFactoryInstance().showRegisterPatientView(e);
 		});
+    	
+    	saveInfoBtn.setOnAction( e-> {
+    		if(firstName.getText().equals(PatientFile.getFileInstance().getFName())
+					&& lastName.getText().equals(PatientFile.getFileInstance().getLName())
+					&&birthDate.getValue().toString().equals(PatientFile.getFileInstance().getDateOfBirth()))
+			{
+    			//setting all values for visit
+    			PatientVisitFile.getFileInstance().setFName(firstName.getText());
+    			PatientVisitFile.getFileInstance().setLName(lastName.getText());
+    			PatientVisitFile.getFileInstance().setDateOfBirth(birthDate.getValue().toString());
+    			PatientVisitFile.getFileInstance().setAllergies(allergiesTA.getText());
+    			PatientVisitFile.getFileInstance().setBloodPressure(bloodPressureTF.getText());
+    			PatientVisitFile.getFileInstance().setHealthConcerns(healthConcernsTA.getText());
+    			PatientVisitFile.getFileInstance().setHeight(heightTF.getText());
+    			PatientVisitFile.getFileInstance().setHistoryOfImmunization(immunizationList.getText());
+    			PatientVisitFile.getFileInstance().setPreviousHealth(prevHealthList.getText());
+    			PatientVisitFile.getFileInstance().setPreviousPrescription(prevMedList.getText());
+    			PatientVisitFile.getFileInstance().setWeight(weightTF.getText());
+    			PatientVisitFile.getFileInstance().setBodyTemp(tempTF.getText());
+    			
+    			
+    			try {
+					PatientVisitFile.getFileInstance().createFile();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+    		}
+    	});
+    	
+    	//finds pateint
+    	findPatientBtn.setOnAction(e->{
+    		if(birthDate.getValue() == null) {
+    			errorLabel.setText("Please select a date");
+    		}
+    		else if(firstName.getText().equals("")) {
+    			errorLabel.setText("Please enter the first name");
+    		}
+    		else if(lastName.getText().equals("")) {
+    			errorLabel.setText("Please enter the last name");
+    		}
+    		else {
+    			try {
+					PatientFile.getFileInstance().accessFile(firstName.getText(), lastName.getText(), birthDate.getValue().toString());
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+    			
+    			//verifies findings
+    			if(firstName.getText().equals(PatientFile.getFileInstance().getFName())
+    					&& lastName.getText().equals(PatientFile.getFileInstance().getLName())
+    					&&birthDate.getValue().toString().equals(PatientFile.getFileInstance().getDateOfBirth()))
+    			{
+ //   				System.out.print("true");
+    				errorLabel.setText("If not found,");
+    				try {
+						PatientVisitFile.getFileInstance().accessFile(firstName.getText(), lastName.getText(), birthDate.getValue().toString());
+					} catch (FileNotFoundException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+    				immunizationList.setText(PatientVisitFile.getFileInstance().getHistoryOfImmunization());
+    				prevMedList.setText(PatientVisitFile.getFileInstance().getPreviousMed() + " " +  PatientVisitFile.getFileInstance().getNewPrescription());
+    				prevHealthList.setText(PatientVisitFile.getFileInstance().getPreviousHealth() + " " +  PatientVisitFile.getFileInstance().getHealthConcerns());
+    				patientName.setText(PatientFile.getFileInstance().getFName() + " " + PatientFile.getFileInstance().getLName());
+    			}
+    			else {
+//    				System.out.print("false");
+    				errorLabel.setText("Patient not found please->");
+    			}
+    		}
+    	});
     }
 }
