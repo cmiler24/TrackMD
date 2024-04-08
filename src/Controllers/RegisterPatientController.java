5f7ca435bed8372cdd0e010a8bb6547d10926787
timkrans33 <93606550+timkrans33@users.noreply.github.com>
2024-04-07 21:19:32 -0700

    Update RegisterPatientController.java
    fixed handlers on buttons

--- src/Controllers/RegisterPatientController.java
+++ src/Controllers/RegisterPatientController.java
@@ -1,11 +1,16 @@
 package Controllers;
 
+import java.io.IOException;
+
+import application.PatientFile;
 import application.ViewFactory;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 
 public class RegisterPatientController {
 
@@ -27,6 +32,9 @@ public class RegisterPatientController {
     @FXML
     private Button registerBtn;
     
+    @FXML
+    private Label  errorLabel;
+    
     public  RegisterPatientController () {
     	
     }
@@ -42,9 +50,33 @@ public class RegisterPatientController {
 			ViewFactory.getViewFactoryInstance().showNurseView(e);
 		});
     	
+    	
+    	//button handler for create nre user
     	registerBtn.setOnAction( e -> {
 			//System.out.print("clicked");
-			ViewFactory.getViewFactoryInstance().showNurseView(e);
-		});
+    		
+    		if(dateOfBirth.getValue() == null) {
+    			errorLabel.setText("Please select a date");
+    		}
+    		else if(firstNameTF.getText().equals("")) {
+    			errorLabel.setText("Please enter the first name");
+    		}
+    		else if(lastNameTF.getText().equals("")) {
+    			errorLabel.setText("Please enter the last name");
+    		}
+    		else {
+    			PatientFile.getFileInstance().setDateOfBirth(dateOfBirth.getValue().toString());
+    			PatientFile.getFileInstance().setFName(firstNameTF.getText());
+    			PatientFile.getFileInstance().setLName(lastNameTF.getText());
+    			System.out.print(firstNameTF.getText());
+    			try {
+					PatientFile.getFileInstance().createFile();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+    			errorLabel.setText("Patient Created Click ->");
+    		}
+    	});
     }
 }
