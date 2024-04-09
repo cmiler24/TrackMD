248e3999bca6aecf78f4ff683881f155cb7929b4
Owen <oeaton116@gmail.com>
2024-04-09 14:02:37 -0700

    Adding new project files

---
+++ src/Controllers/MessagePortalController.java
@@ -0,0 +1,25 @@
+package Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+
+public class MessagePortalController {
+
+    @FXML
+    private ListView<?> contactList;
+
+    @FXML
+    private TextArea inputMsgBtn;
+
+    @FXML
+    private Button logOutBtn;
+
+    @FXML
+    private ListView<?> messagesListView;
+
+    @FXML
+    private Button sendBtn;
+
+}
